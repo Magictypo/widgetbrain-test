e8ebc617f966cc8215d09a7f9306ca5c5c0b6885
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-13 14:46:52 +0700

    update header footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,37 +10,7 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-      <ul class="navbar-nav mr-auto">
-        <li>
-          <router-link
-            class="nav-link"
-            :to="{ name: 'home' }">
-            Home
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'about' }">
-            About
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'login' }">
-            Login
-          </router-link>
-        </li>
-        <li class="nav-item">
-          <router-link
-            class="nav-link"
-            :to="{ name: 'asset-health' }">
-            Asset Health
-          </router-link>
-        </li>
-      </ul>
-      <ul class="navbar-nav">
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown"
              aria-haspopup="true" aria-expanded="false">Dropdown</a>
