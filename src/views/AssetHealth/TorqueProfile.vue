860b9e551ecf39a7e8424f8e9e5f2b9b3a3d3f04
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-13 14:11:08 +0700

    Add Forecase

--- src/views/AssetHealth/TorqueProfile.vue
+++ src/views/AssetHealth/TorqueProfile.vue
@@ -59,13 +59,18 @@ export default {
         if (res) {
           const AverageSeriesOpen = _.find(this.OpenChartOptions.series, { name: 'Average' });
           const LastSeriesOpen = _.find(this.OpenChartOptions.series, { name: 'Last' });
+          const ForeCastSeriesOpen = _.find(this.OpenChartOptions.series, { name: 'Forecast' });
+
           const AverageSeriesClose = _.find(this.CloseChartOptions.series, { name: 'Average' });
           const LastSeriesClose = _.find(this.CloseChartOptions.series, { name: 'Last' });
+          const ForeCastSeriesClose = _.find(this.CloseChartOptions.series, { name: 'Forecast' });
 
           const AverageArrayOpen = [];
           const LastArrayOpen = [];
+          const ForecastArrayOpen = [];
           const AverageArrayClose = [];
           const LastArrayClose = [];
+          const ForecastArrayClose = [];
 
           // eslint-disable-next-line no-plusplus
           for (let position = 1; position <= 100; position++) {
@@ -83,23 +88,43 @@ export default {
             if (LastPositionArrayOpen) {
               AverageArrayOpen.push(LastPositionArrayOpen.AverageTorque);
               LastArrayOpen.push(LastPositionArrayOpen.LastTorque);
+              ForecastArrayOpen.push(
+                (
+                  LastPositionArrayOpen.AverageTorque
+                  + LastPositionArrayOpen.LastTorque
+                )
+                  / 2,
+              );
             } else {
               AverageArrayOpen.push(0);
               LastArrayOpen.push(0);
+              ForecastArrayOpen.push(0);
             }
+
             if (LastPositionArrayClose) {
               AverageArrayClose.push(LastPositionArrayClose.AverageTorque);
               LastArrayClose.push(LastPositionArrayClose.LastTorque);
+              ForecastArrayClose.push(
+                (
+                  LastPositionArrayClose.AverageTorque
+                  + LastPositionArrayClose.LastTorque
+                )
+                / 2,
+              );
             } else {
               AverageArrayClose.push(0);
               LastArrayClose.push(0);
+              ForecastArrayClose.push(0);
             }
           }
 
           AverageSeriesOpen.data = AverageArrayOpen;
           LastSeriesOpen.data = LastArrayOpen;
+          ForeCastSeriesOpen.data = ForecastArrayOpen;
+
           AverageSeriesClose.data = AverageArrayClose;
           LastSeriesClose.data = LastArrayClose;
+          ForeCastSeriesClose.data = ForecastArrayClose;
         }
       } catch (e) {
         ErrorSvc.getError(e);
