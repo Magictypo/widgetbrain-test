6b61659b4b18e2d4252ac64665b14900af615a14
Jonathan Ephraim Edwin <joedwin.rpl@gmail.com>
2020-07-16 00:54:02 +0700

    hotfix bug, only init mock in dev

--- src/mock.js
+++ src/mock.js
@@ -2,10 +2,8 @@ import MockAdapter from 'axios-mock-adapter';
 import axios from 'axios';
 import torqueResponse from '@/resources/mock/getTorqueProfile.json';
 
-const mock = new MockAdapter(axios);
-const enable = true;
-
-if (process.env.NODE_ENV === 'development' && enable) {
+if (process.env.NODE_ENV === 'development') {
+  const mock = new MockAdapter(axios);
   mock
     .onGet('https://b507qiqddb.execute-api.eu-central-1.amazonaws.com/torque')
     .reply(200, torqueResponse);
